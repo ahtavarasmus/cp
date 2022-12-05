7f8aa1aecd882e063670673f4c3de5e31b195e90
Rasmus <rasmus.ahtava@tuni.fi>
2022-12-05 20:33:53 +0200

    change

---
+++ adventofcode2022/day5/p1/problem.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x)
+#endif
+
+void solve(){
+    string line;
+    int len,stack_num;
+    map<int,deque<char> > stacks;
+    // reading stacks
+    while (true){
+        getline(cin,line);
+        len = line.size();
+        if (!len) break;
+        stack_num = 1;
+        for (int i = 0; i < len; ++i){
+            if (line[i] == '['){
+                debug(line[i+1])
+                stacks[stack_num].push_back(line[i+1]);
+            } 
+            ++stack_num;
+            i += 3;
+        }
+        debug(line)
+    }
+    debug("HAHAHAHAHHAAHHAHAHAHAHHAHHAAHHAHAHAHAH")
+
+
+    // reading moves
+    string com;
+    int count,from,to;
+    char a,b;
+    while (true){
+        getline(cin,line);
+        len = line.size();
+        if (!len) break;
+        istringstream linestream(line);
+        linestream >> com >> count >> com >> from >> com >> to;
+        while (count--){
+            a = stacks[from].front();
+            stacks[from].pop_front();
+            stacks[to].push_front(a);
+        }
+    }
+    string message;
+    for (auto tops : stacks){
+        message.push_back(tops.second.front());
+    }
+    cout << message;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+  
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+    solve();
+    cout << "\n";
+  
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
