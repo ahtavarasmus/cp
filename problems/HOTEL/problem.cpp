3aaa789bff3f309665f27bcfb7def70ae89d4303
Rasmus <ahtavarasmus@gmail.com>
2022-12-19 10:22:50 +0200

    change

---
+++ problems/HOTEL/problem.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x)
+#endif
+
+void solve(){
+    map<int,int> hotel;
+    int n,time;
+    cin >> n;
+
+    int a[n];
+    map<int,int> quests;
+    for (int i = 0;  i < n; ++i){
+        cin >> time;
+        quests[time]++;
+    }
+    for (int i = 0; i < n; ++i){
+        cin >> time;
+        quests[time]--;
+    }
+    int count = 0;
+    int max_count = 0;
+    for (auto & q : quests){
+        count += q.second;
+        max_count = max(max_count,count);
+    }
+    cout << max_count;
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+  
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+  
+    int t = 1;
+    /*is Single Test case?*/ cin >> t;
+    while (t--) {
+        solve();
+        cout << "\n";
+    }
+  
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
