266a708726ff9d1fae51b5c2456a71e40acabe3f
Rasmus <ahtavarasmus@gmail.com>
2023-01-01 13:35:31 +0200

    change

--- problems/HOTEL/problem.cpp
+++ problems/HOTEL/problem.cpp
@@ -12,20 +12,25 @@ void solve(){
     int n,time;
     cin >> n;
 
+    int max_time = INT_MIN,min_time = INT_MAX;
+
     int a[n];
+    // time,count
     map<int,int> quests;
     for (int i = 0;  i < n; ++i){
         cin >> time;
+        min_time = min(min_time,time);
         quests[time]++;
     }
     for (int i = 0; i < n; ++i){
         cin >> time;
+        max_time = max(max_time,time);
         quests[time]--;
     }
     int count = 0;
     int max_count = 0;
-    for (auto & q : quests){
-        count += q.second;
+    for (int i = min_time; i <= max_time; ++i){
+        count += quests[i];
         max_count = max(max_count,count);
     }
     cout << max_count;
