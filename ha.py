48ad3942f0115f3bca58518b0b7a43b64308059b
Rasmus <ahtavarasmus@gmail.com>
2023-09-06 22:30:54 +0300

    change

--- ha.py
+++ ha.py
@@ -5,11 +5,6 @@ def binary_search(list, item):
     while low <= high:
         mid = (low+high)/2
         guess = list[mid]
-        if guess == item:
-            return mid
-        if guess > item:
-            high = mid-1
-        else:
-            low = mid+1
-    return None
+        return None
+#this is just a commit test
 
